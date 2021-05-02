4504d1628d83beb9ab1c90f7ce89152d61488241
Konstantin <shishmaryovkonstantin@gmail.com>
2021-05-02 19:38:16 +0300

    base styles

---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 15000
+  });
+});
