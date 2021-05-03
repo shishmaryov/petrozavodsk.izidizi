2e791f2a46ea3b13cb43a0b9fcb3772fe74df219
Konstantin <shishmaryovkonstantin@gmail.com>
2021-05-03 12:54:45 +0300

    desktop styles without popups

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
     autoplaySpeed: 15000
   });
 });
+
+$(document).ready(function(){
+  $('.nav__link').click(function(e){
+      if($(this).attr('href').indexOf('#') != -1){
+      e.preventDefault();
+            var href = $(this).attr('href').replace('#', '');
+      if($('#'+href).length > 0){
+              var tophref = $('body').find('#'+href).offset().top;
+              $('html, body').animate({scrollTop: tophref}, 800);
+      }
+    }
+    });
+});
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() < 500) {
+      $('.header--fixed').addClass('disable');
+  } else {
+    $('.header--fixed').removeClass('disable');
+  }
+});
